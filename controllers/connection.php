5544c7e7338ad4b82223b0e5a4aba3e63b64c6a8
Felix Teoh <felixteoh98@gmail.com>
2021-01-29 00:11:14 +0800

    second commit

--- controllers/connection.php
+++ controllers/connection.php
@@ -1,15 +1,15 @@
 <?php  
 
 //LOCAL DATABSE
-$host = "localhost";
-$username = "root";
-$password = "";
-$name = "fishapp_ecommerce";
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $name = "fishapp_ecommerce";
 
 //ONLINE DATABASE
-// $host = "db4free.net";
-// $username = "b2ecomfelix";
-// $password = "123123123123";
-// $name = "b2ecomfelix";
+$host = "db4free.net";
+$username = "b2goldfish";
+$password = "123123123123";
+$name = "fishapp_ecommerce";
 
 $cn = new mysqli($host, $username, $password, $name);
